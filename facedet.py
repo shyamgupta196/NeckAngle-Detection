0a33315872f5a62bcec73123d44fdd502362f4e8
shyamgupta196 <60971003+shyamgupta196@users.noreply.github.com>
2022-05-02 20:09:14 +0530

    Add files via upload
    Image face det karne ki koshish , rectangles not drawn !! trying to introduce tensor and imagefolder in next update

--- facedet.py
+++ facedet.py
@@ -1,10 +1,12 @@
 import cv2
-from facenet_pytorch import MTCNN
+from facenet_pytorch import MTCNN,InceptionResnetV1
 import torch
 import numpy as np
 import mmcv
 from PIL import Image, ImageDraw
 from IPython import display
+import sys
+import matplotlib.pyplot as plt
 
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 print('Running on device: {}'.format(device))
@@ -14,7 +16,9 @@ cascPath = 'facedet.xml'
 
 faceCascade = cv2.CascadeClassifier(cascPath)
 
-mtcnn = MTCNN(keep_all=True, device=device)
+mtcnn = MTCNN(image_size=160,keep_all=True, device=device)
+
+resnet = InceptionResnetV1(pretrained='vggface2').eval()
 
 cap = cv2.VideoCapture(0)
 
@@ -33,25 +37,19 @@ while 1:
     #         minNeighbors=5,
     #         minSize=(30, 30),
     #         flags=cv2.CASCADE_SCALE_IMAGE)
+    print(img.shape)
     ear = ear_cascade.detectMultiScale(img, 1.3, 5)
-    cv2.imshow('img',img)
-    cv2.waitKey(300)
-    frames_tracked = []
-    for i, frame in enumerate(img):
-        print('\rTracking frame: {}'.format(i + 1), end='')
-        # print(type(boxes))
-        # print(boxes[0])
-        
-        # Detect faces
-        boxes, _ = mtcnn(frame)
-        # Draw faces
-        frame_draw = frame.copy()
-        draw = ImageDraw.Draw(frame_draw)
-        for box in boxes:
-            cv2.rectangle(img,box.tolist(), color=(255, 0, 0), width=6)
-        
+    face = mtcnn(img)  
+  
+    # for box in boxes:
+    #     cv2.rectangle(img,box.tolist(), color=(255, 0, 0), width=6)
+    print(face.shape)
+    
+    cv2.imshow(face.permute(1, 2, 0).int().numpy())
+    
         # Add to frame list
-        frames_tracked.append(frame_draw.resize((640, 360), Image.BILINEAR))
+        
+        # frames_tracked.append(frame_draw.resize((640, 360), Image.BILINEAR))
     
     
     for (x,y,w,h) in ear:
