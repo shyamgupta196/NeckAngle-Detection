b7649aa80be80b969bbad1a1f2f2b675ebc7fb7f
shyamgupta196 <60971003+shyamgupta196@users.noreply.github.com>
2022-05-15 23:36:19 +0530

    detects only when face sees left
    works completely fine for left , may add a mouse pointer cv2 object for manual work ! and angle det

--- facedet.py
+++ facedet.py
@@ -1,14 +1,11 @@
+from tkinter import font
 import cv2
-from facenet_pytorch import MTCNN,InceptionResnetV1
-from torch.utils.data import DataLoader
-
+from facenet_pytorch import MTCNN
+import math
 import torch
 import numpy as np
-import mmcv
-from torchvision import datasets,transforms
-from PIL import Image, ImageDraw
-from IPython import display
-import sys
+from PIL import Image, ImageDraw, ImageFont
+
 import matplotlib.pyplot as plt
 
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
@@ -19,30 +16,80 @@ cascPath = 'facedet.xml'
 
 faceCascade = cv2.CascadeClassifier(cascPath)
 
-mtcnn = MTCNN(device=device,keep_all=False)
+mtcnn = MTCNN(device=device, keep_all=True)
 
-img = Image.open('images/faces/friends.jpg')
-
-# cap = cv2.VideoCapture(0)
-
-
+img = Image.open('images/im1.jpg')
 
 
 print(img.size)
+
+
+# def detect(img):
 boxes, _ = mtcnn.detect(img)
 imgc = img.copy()
 draw = ImageDraw.Draw(imgc)
-for box in boxes:
-    draw.rectangle(box.tolist(), outline=(255, 0, 0), width=3)
+
+a = boxes[0][0]
+b = boxes[0][1]
+c = boxes[0][2]
+d = boxes[0][3]
+
+draw.rectangle((a, b, c, d), width=2)
+print(boxes)
 plt.imshow(imgc)
 plt.show()
 
-imgarr = np.array(imgc)
-ear = ear_cascade.detectMultiScale(imgarr,minNeighbors=5,scaleFactor=1.15)
+img1 = Image.open('images/im2.jpg')
 
-for (x,y,w,h) in ear:
-    cv2.rectangle(imgarr, (x,y), (x+w,y+h), (255,0,0), 3)
+boxes, _ = mtcnn.detect(img1)
+imgc1 = img1.copy()
+draw = ImageDraw.Draw(imgc1)
 
-cv2.imshow('img',imgarr)
-plt.imshow(imgarr)
+e = boxes[0][1]
+f = boxes[0][2]
+g = boxes[0][3]
+h = boxes[0][0]
+
+print(boxes)
+
+draw.line((c, d, a, d), fill=(255, 0, 0), width=2)
+draw.line((c, d, f, e), fill=(255, 0, 0), width=4)
+draw.line((c, d, c, b), fill=(255, 0, 0), width=2)
+
+
+def slope(p1, p2):
+    return (p2[1]-p1[1])/(p2[0]-p1[0])
+
+
+x1 = [c, d]
+x2 = [a, d]
+x3 = [f, e]
+
+font = ImageFont.truetype("arial.ttf", 100)
+m1 = slope(x1, x2)
+m2 = slope(x1, x3)
+print(m1, m2)
+print('8'*10)
+
+angle = math.atan((m2-m1)/1+m1*m2)
+
+print('angle', (m2-m1)/1+m1*m2)
+print('radian', angle)
+angle = round(math.degrees((angle)))
+angle = 90-angle
+
+print('round angle :', angle)
+
+draw.text((c, d), text=f'{angle}', color=(0, 0, 255), font=font)
+
+fig, ax = plt.subplots(1, 2)
+
+
+ax[0].imshow(imgc)
+ax[1].imshow(imgc1)
+# plt.imshow(imgc1)
+ax[0].set_xticklabels([])
+ax[1].set_xticklabels([])
+plt.tight_layout()
+
 plt.show()
